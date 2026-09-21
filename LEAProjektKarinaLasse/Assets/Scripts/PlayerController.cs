6af35b0264aab4de18199b447d943318429139ed
Lasse <Lasse.Krumm@edu.bib.de>
2026-09-21 10:09:45 +0200

    Player und Camera

---
+++ LEAProjektKarinaLasse/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform head;
+    public float playerSpeed = 5f;
+    public float playerAcceleration = 10f;
+    private Rigidbody rb;
+    private Vector3 direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal") * head.right + Input.GetAxis("Vertical") * head.forward;
+        rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, direction.normalized * playerSpeed + rb.linearVelocity.y * Vector3.up, playerAcceleration * Time.deltaTime);
+    }
+}
