f47e8e653750851bca9ff316eb4b2402970c32d1
Lasse <Lasse.Krumm@edu.bib.de>
2026-09-21 14:23:34 +0200

    bombo

--- LEAProjektKarinaLasse/Assets/Scripts/CameraController.cs
+++ LEAProjektKarinaLasse/Assets/Scripts/CameraController.cs
@@ -12,7 +12,7 @@ public class CameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      Cursor.lockState = CursorLockMode.Locked;
+      Cursor.lockState = CursorLockMode.Locked;   
     }
 
     // Update is called once per frame
--- LEAProjektKarinaLasse/Assets/Scripts/PlayerController.cs
+++ LEAProjektKarinaLasse/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Light spotlight;
     private Rigidbody rb;
     private Vector3 direction;
+    private Vector3 targetVelocity;
 
     void Start()
     {
@@ -17,7 +18,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction = Input.GetAxis("Horizontal") * head.right + Input.GetAxis("Vertical") * head.forward;
-        rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, direction.normalized * playerSpeed + rb.linearVelocity.y * Vector3.up, playerAcceleration * Time.deltaTime);
+        
+        // Zielgeschwindigkeit basierend auf Input berechnen
+        targetVelocity = direction.normalized * playerSpeed + rb.linearVelocity.y * Vector3.up;
+        
+        // Nur wenn es Input gibt, zur Zielgeschwindigkeit interpolieren
+        // Ansonsten zur Null-Velocity (kein horizontaler Input = stillstehen)
+        if (direction.magnitude > 0.01f)
+        {
+            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, targetVelocity, playerAcceleration * Time.deltaTime);
+        }
+        else
+        {
+            // Kein Input: Horizontal abbremsen, aber Gravity beibehalten
+            Vector3 stoppedVelocity = new Vector3(0, rb.linearVelocity.y, 0);
+            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, stoppedVelocity, playerAcceleration * Time.deltaTime);
+        }
 
         if (Input.GetKeyDown(KeyCode.F))
         {
