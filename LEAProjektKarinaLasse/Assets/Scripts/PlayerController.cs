8821182a0be29dbda34329b34804ffed1c1217c1
Lasse <Lasse.Krumm@edu.bib.de>
2026-09-21 11:19:55 +0200

    Flashlight an/aus mit F

--- LEAProjektKarinaLasse/Assets/Scripts/PlayerController.cs
+++ LEAProjektKarinaLasse/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public Transform head;
     public float playerSpeed = 5f;
     public float playerAcceleration = 10f;
+    public Light spotlight;
     private Rigidbody rb;
     private Vector3 direction;
 
@@ -17,5 +18,10 @@ public class PlayerController : MonoBehaviour
     {
         direction = Input.GetAxis("Horizontal") * head.right + Input.GetAxis("Vertical") * head.forward;
         rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, direction.normalized * playerSpeed + rb.linearVelocity.y * Vector3.up, playerAcceleration * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            spotlight.enabled = !spotlight.enabled;
+        }
     }
 }
